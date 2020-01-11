c4c74e0f00c5b1e95eef9c6b22d3ea75d3c8b626
Jett Kim <jett.kim@gmail.com>
2020-01-11 15:13:27 -0800

    Added color sensor functionality
    -Created and added color sensor to ControlPanelSubsystem
    -Created ControlPanelColor enumerator
    -Created ColorMatcher utility

---
+++ src/main/java/frc/robot/subsystems/controlpanel/ControlPanelSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems.controlpanel;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RobotMap;
+import frc.robot.types.ControlPanelColor;
+import frc.robot.utilities.ColorMatcher;
+
+/**
+ * ControlPanelSubsystem handles the control panel manipulator and sensor.
+ */
+public class ControlPanelSubsystem extends SubsystemBase {
+  private final ColorSensorV3 m_colorSensor;
+  private final ColorMatcher m_colorMatcher;
+  private ControlPanelColor m_matchedColor;
+
+  private final CANSparkMax m_motor;
+  private final CANEncoder m_encoder;
+
+  public ControlPanelSubsystem() {
+    m_colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+    m_colorMatcher = new ColorMatcher();
+    m_motor = new CANSparkMax(RobotMap.kControlPanelSparkMax, MotorType.kBrushless);
+    m_encoder = m_motor.getEncoder();
+  }
+
+  @Override
+  public void periodic() {
+    Color detectedColor = m_colorSensor.getColor();
+    m_matchedColor = m_colorMatcher.getMatchedColor(detectedColor);
+    SmartDashboard.putString("Matched Color", m_matchedColor.name());
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+  }
+
+  public ControlPanelColor getMatchedColor() {
+    return m_matchedColor;
+  }
+}
