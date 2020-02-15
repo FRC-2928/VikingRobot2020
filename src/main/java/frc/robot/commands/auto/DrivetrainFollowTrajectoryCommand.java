3370cb823b56cccf88dbcf641036aa1ba6b747d0
mjwhite8119 <11601920+mjwhite8119@users.noreply.github.com>
2020-02-15 12:04:30 -0800

    Moved drivetrain trajectory command

--- src/main/java/frc/robot/commands/auto/DrivetrainFollowTrajectoryCommand.java
+++ src/main/java/frc/robot/commands/auto/DrivetrainFollowTrajectoryCommand.java
@@ -1,17 +1,11 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
-package frc.robot.subsystems.drivetrain;
+package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.subsystems.drivetrain.DrivetrainSubsystem;
 
 public class DrivetrainFollowTrajectoryCommand extends RamseteCommand {
   /**
