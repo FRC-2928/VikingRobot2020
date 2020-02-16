59b212dc55e209fe228cb0cd41342f78b928c778
YumiHaskins <yumi@ayne.net>
2020-02-16 15:15:27 -0800

    Update leds.java

--- src/main/java/frc/robot/subsystems/leds/leds.java
+++ src/main/java/frc/robot/subsystems/leds/leds.java
@@ -4,17 +4,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotMap;
 import frc.robot.utilities.LEDS;
 
+
+
 public class leds extends SubsystemBase {
-  /**
-   * Creates a new ExampleSubsystem.
-   */
+  
+   private final LEDS m_LEDS;
+   
   public leds() {
+
+    m_LEDS = new LEDS();
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    frc.robot.utilities.LEDS.allianceColor    allianceColor() 
+    m_LEDS.allianceColor();
+  }
+
+  //still needs to be called repeatedly to work
+  @Override
+  public void displayRainbow() {
+      m_LEDS.rainbow();
   }
 }
